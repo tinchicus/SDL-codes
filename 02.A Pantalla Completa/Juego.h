2ca83633fd20d417cbee1a48ccc6791a0b079a09
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-06-26 17:53:43 -0300

    Add files via upload

---
+++ 02.A Pantalla Completa/Juego.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef __Juego__
+#define __Juego__
+
+#include <SDL3/SDL.h>
+
+class Juego
+{
+public:
+	Juego() {}
+	~Juego() {}
+	bool iniciar(const char*, int, int,bool);
+	void renderizar();
+	void actualizar();
+	void manejaEventos();
+	void limpiar();
+
+	bool corriendo();
+
+private:
+	SDL_Window* m_pVentana;
+	SDL_Renderer* m_pRenderer;
+	bool m_bCorriendo;
+};
+
+#endif /* defined(__Juego__) */
