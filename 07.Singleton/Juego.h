ec015ef369cb676e0cf7a0742f2c08f58e8a2ef2
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-06-27 14:11:29 -0300

    Add files via upload

---
+++ 07.Singleton/Juego.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __Juego__
+#define __Juego__
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_surface.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3_image/SDL_image.h>
+#include "ManejarTexturas.h"
+
+class Juego
+{
+public:
+	Juego() {}
+	~Juego() {}
+	bool iniciar(const char*, int, int,bool);
+	void renderizar();
+	void actualizar();
+	void manejaEventos();
+	void limpiar();
+
+	bool corriendo();
+
+private:
+	SDL_Window* m_pVentana;
+	SDL_Renderer* m_pRenderer;
+	bool m_bCorriendo;
+	int m_frameActual;
+};
+
+#endif /* defined(__Juego__) */
