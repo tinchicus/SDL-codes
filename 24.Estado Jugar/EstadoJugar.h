de7efa6680012a61cd502cec231ab985e2b0d03c
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-07-12 11:36:45 -0300

    Add files via upload

---
+++ 24.Estado Jugar/EstadoJugar.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef __EstadoJugar__
+#define __EstadoJugar__
+#include "EstadoJuego.h"
+#include <vector>
+#include "ObjetoJuego.h"
+#include "ManejarTexturas.h"
+#include "Juego.h"
+
+class EstadoJugar : public EstadoJuego
+{
+public:
+	virtual void actualizar();
+	virtual void renderizar();
+	virtual bool enIngreso();
+	virtual bool enSalida();
+	virtual std::string getIdEstado() const;
+
+private:
+	static const std::string e_idjugar;
+	std::vector<ObjetoJuego*> m_objetosJuego;
+};
+
+#endif // !__EstadoJugar__
---
+++ 24.Estado Jugar/MaquinaEstadoJuego.cpp
@@ -0,0 +1,42 @@
+#include "MaquinaEstadoJuego.h"
+
+void MaquinaEstadoJuego::pushEstado(EstadoJuego* pEstado) {
+	m_estadoJuego.push_back(pEstado);
+	m_estadoJuego.back()->enIngreso();
+}
+
+void MaquinaEstadoJuego::popEstado() {
+	if (!m_estadoJuego.empty()) {
+		if (!m_estadoJuego.back()->enSalida()) {
+			delete m_estadoJuego.back();
+			m_estadoJuego.pop_back();
+		}
+	}
+}
+
+void MaquinaEstadoJuego::cambiaEstado(EstadoJuego* pEstado) {
+	if (!m_estadoJuego.empty()) {
+		if (m_estadoJuego.back()->getIdEstado() ==
+			pEstado->getIdEstado()) {
+			return;
+		}
+		if (m_estadoJuego.back()->enSalida()) {
+			delete m_estadoJuego.back();
+			m_estadoJuego.pop_back();
+		}
+	}
+	m_estadoJuego.push_back(pEstado);
+	m_estadoJuego.back()->enIngreso();
+}
+
+void MaquinaEstadoJuego::actualizar()
+{
+	if (!m_estadoJuego.empty())
+		m_estadoJuego.back()->actualizar();
+}
+
+void MaquinaEstadoJuego::renderizar()
+{
+	if (!m_estadoJuego.empty())
+		m_estadoJuego.back()->renderizar();
+}
