33d61cbcf5b4e71d998c8e88cef25c481f3073c0
tinchicus <126396777+tinchicus@users.noreply.github.com>
2025-07-17 21:29:30 -0300

    Add files via upload

---
+++ 29.Fabrica de objetos/CargadorParams.cpp
@@ -0,0 +1,17 @@
+#include "CargadorParams.h"
+
+CargadorParams::CargadorParams(float x, float y,
+	float ancho, float alto, std::string id, int numFrames,
+	int idCallback, int velocAnim) :
+	m_x(x), m_y(y), m_ancho(ancho), m_alto(alto),
+	m_idTextura(id), m_numFrames(numFrames),
+	m_idCallback(idCallback), m_velocAnim(velocAnim) {
+}
+
+float CargadorParams::getX() const { return m_x; }
+float CargadorParams::getY() const { return m_y; }
+float CargadorParams::getAncho() const { return m_ancho; }
+float CargadorParams::getAlto() const { return m_alto; }
+int CargadorParams::getNumFrames() const { return m_numFrames; }
+
+std::string CargadorParams::getIdTextura() const { return m_idTextura; }
---
+++ 29.Fabrica de objetos/EstadoPausa.cpp
@@ -0,0 +1,56 @@
+#include "EstadoPausa.h"
+#include <Windows.h>
+
+const std::string EstadoPausa::e_idpausa = "PAUSA";
+
+void EstadoPausa::e_volverMenu() {
+	Eljuego::instanciar()->getEstadoMaquina()->cambiaEstado(new EstadoMenu());
+}
+
+void EstadoPausa::e_retomarJuego() {
+	Eljuego::instanciar()->getEstadoMaquina()->popEstado();
+}
+
+void EstadoPausa::actualizar() {
+	for (int i = 0; i < m_objetosJuego.size(); i++)
+		m_objetosJuego[i]->actualizar();
+}
+
+void EstadoPausa::renderizar() {
+	for (int i = 0; i < m_objetosJuego.size(); i++)
+		m_objetosJuego[i]->dibujar();
+}
+
+bool EstadoPausa::enIngreso() {
+	if (!Elmanejador::instanciar()->cargar("assets/retomar.png",
+		"botonretomar", Eljuego::instanciar()->getRenderer())) {
+		return false;
+	}
+	if (!Elmanejador::instanciar()->cargar("assets/volver.png",
+		"botonvolver", Eljuego::instanciar()->getRenderer())) {
+		return false;
+	}
+	ObjetoJuego* boton1 = new BotonMenu(new CargadorParams(100, 100,
+		400, 100, "botonvolver"), e_volverMenu);
+	ObjetoJuego* boton2 = new BotonMenu(new CargadorParams(100, 300,
+		400, 100, "botonretomar"), e_retomarJuego);
+	m_objetosJuego.push_back(boton1);
+	m_objetosJuego.push_back(boton2);
+
+	OutputDebugStringA("Entrando en estado Pausa\n");
+	return true;
+}
+
+bool EstadoPausa::enSalida() {
+	for (int i = 0; i < m_objetosJuego.size(); i++)
+		m_objetosJuego[i]->limpiar();
+	m_objetosJuego.clear();
+	Elmanejador::instanciar()->limpiaMapaTexturas("botonretomar");
+	Elmanejador::instanciar()->limpiaMapaTexturas("botonvolver");
+	Controles::instanciar()->limpiar();
+
+	OutputDebugStringA("Saliendo del estado Pausa\n");
+	return true;
+}
+
+std::string EstadoPausa::getIdEstado() const { return e_idpausa; }
